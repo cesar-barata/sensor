68b6c161b96e39eed7b27b23098977ae76e40fa3
Cesar Barata <cesar.adbarata@gmail.com>
2020-02-06 23:51:52 -0300

    Set up basic project dependencies and implemented some parsing functions.

---
+++ src/test/scala/ReadingParserSpec.scala
@@ -0,0 +1,25 @@
+import org.scalatest.FlatSpec
+
+import scala.util.{Failure, Success}
+
+class ReadingParserSpec extends FlatSpec {
+  "parseSensorId" should "return an integer wrapped in a Success" in {
+    val expected = Success(23)
+    val actual = Reading.parseSensorId("s23")
+    assert(expected == actual)
+  }
+
+  it should "return a Failure indicating a missing \"s\" prefix" in {
+    val sourceString = "123"
+    val expected = Failure(ParserError(s"String '${sourceString}' does not start with an 's'"))
+    val actual = Reading.parseSensorId(sourceString)
+    assert(expected == actual)
+  }
+
+  it should "return a Failure indicating an integer could not be parsed from source string" in {
+    val sourceString = "sabc"
+    val expected = Failure(ParserError(s"Couldn't parse an integer for sensor ID from string '${sourceString}'"))
+    val actual = Reading.parseSensorId(sourceString)
+    assert(expected == actual)
+  }
+}
